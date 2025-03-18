157bcc86f1529cf33c15cdff35e39197bbfe53d2
Keller Martinez <kmartinez@hiberus.com>
2025-03-18 21:21:08 +0100

    Added tailwindcss and menu

---
+++ src/views/admin/NewProductView.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Link from '@/components/Link.vue';
+</script>
+
+<template>
+    <div>
+        <Link
+            to="admin-products"
+        >
+            Volver
+        </Link>
+    </div>
+    <h1 class="text-4xl font-black my-10">Nuevo Producto</h1>
+</template>
+
---
+++ src/views/admin/ProductsView.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Link from '@/components/Link.vue';
+</script> 
+
+<template>
+    <div>
+        <Link
+            to="admin-new-product"
+        >
+            Nuevo Producto
+        </Link>
+    </div>
+    <h1 class="text-4xl font-black my-10">Productos</h1>
+</template>
+
